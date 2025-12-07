a5a1f95debed2861ec5eb9c2a116497ae8b9af5c
sahar-khiari <sahar.khiari@esprit.tn>
2025-12-07 21:17:42 +0100

    push++-+

--- src/main/java/tn/esprit/studentmanagement/controllers/StudentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.studentmanagement.entities.Student;
 import tn.esprit.studentmanagement.services.IStudentService;
+import tn.esprit.studentmanagement.services.IStudentService;
 
 import java.util.List;
 
