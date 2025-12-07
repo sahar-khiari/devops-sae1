23be8ad974f2bed57d9669c3b907222e72d622dd
sahar-khiari <sahar.khiari@esprit.tn>
2025-12-07 20:53:18 +0100

    push--+

--- src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.studentmanagement.entities.Department;
 import tn.esprit.studentmanagement.entities.Enrollment;
-import tn.esprit.studentmanagement.services.DepartmentService;
+
 import tn.esprit.studentmanagement.services.IDepartmentService;
 
 import java.util.List;
