0ebf1ae485120353153b892f24f13dbd528c7d24
sahar-khiari <sahar.khiari@esprit.tn>
2025-12-07 20:08:53 +0100

    projet--

--- src/main/java/tn/esprit/tp_foyer/entity/TypeChambre.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.tp_foyer.entity;
-
-public enum TypeChambre {
-    SIMPLE, DOUBLE, TRIPLE
-}
--- src/main/java/tn/esprit/tp_foyer/repositories/ChambreRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.tp_foyer.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.tp_foyer.entity.*;
-@Repository
-public interface ChambreRepository    extends JpaRepository<Chambre, Long> {}
